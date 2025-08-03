074302bf1e5a14f864c0615b74b5c30ac32e9cf9
İsmail Durcan <ismaildurcan40@gmail.com>
2025-08-03 16:14:49 +0300

    Try jwt token creation

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const router = express.Router();
+
+jwt.sign;
 
 // /v1/api/auth/login
 router.post("/login", (req, res) => {
-  res.json({ message: "Login endpoint" });
+  const token = jwt.sign(
+    { userId: 15, email: "example@gamil.com" },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.json({
+    message: "Authentication successful",
+    token: token,
+    userId: 15,
+    expiresIn: 3600, // 1 saat saniye cinsinden});
+  });
 });
 
 // /v1/api/auth/register

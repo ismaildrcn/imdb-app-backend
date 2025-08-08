e66eb71563355fd44721655d612a58cd525f9c62
İsmail Durcan <ismaildurcan40@gmail.com>
2025-08-08 21:38:48 +0300

    Add Middlewares

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// CORS middleware (opsiyonel)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Ana route'ları import et
 const router = require("./routes/v1/api");
